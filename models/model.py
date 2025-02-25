d34bdec9c54bf1220369c73ba518c07253e12de6
Claudio Pioggia <claudio.pioggia@gmail.com>
2025-02-25 22:41:39 +0100

    wip

--- models/model.py
+++ models/model.py
@@ -2,15 +2,17 @@
 from pydantic import BaseModel, HttpUrl,Field
 from uuid import UUID
 from datetime import datetime
+from typing import Optional
+
 
 class ModelResponse(BaseModel):
     id: str = Field(alias='_id')
     video_uri: str
-    thumbnail_s3_key: str
-    thumbnail_url: str
+    thumbnail_s3_key: Optional[str] = None
+    thumbnail_url: Optional[str] = None
     title: str
-    output_s3_key: str
-    output_url: str
+    output_s3_key: Optional[str] = None
+    output_url: Optional[str] = None
     status: str
     created_at: datetime
-    updated_at: datetime
+    updated_at: Optional[datetime] = None
--- services/queue_job_consumer_service.py
+++ services/queue_job_consumer_service.py
@@ -10,6 +10,7 @@ import asyncio
 import requests
 import cv2
 import shutil
+import numpy as np
 
 model_service = ModelService()
 repository_service = RepositoryService()
@@ -61,81 +62,85 @@ class QueueJobService:
             ch.basic_ack(delivery_tag=method.delivery_tag)
             return
 
-        # Aggiorna stato "VIDEO_PROCESSING" mentre il video viene processato
-        model_service.update_model_status(model_id, {"status": "VIDEO_PROCESSING"})
 
         try:
             # Passo 2: Creazione delle cartelle di lavoro e download del video
             model_dir = os.path.join(WORKING_DIR, f"{model_id}")
             os.makedirs(model_dir, exist_ok=True)
 
-            video_file_path = os.path.join(model_dir, "video.mp4")
-            repository_service.download(model.video_uri, video_file_path)
+            model_service.update_model_status(model_id, {"status": "VIDEO_PROCESSING"})
 
-            # Estrarre fotogrammi dal video
-            frames_output_folder = os.path.join(model_dir, 'input')
-            thumbnail_path = self.extract_frames(video_file_path, frames_output_folder)
+            thumbnail_s3_key = self.process_video(model_id,model.video_uri,model_dir)
+            
+            model_service.update_model_status(model_id, {"status": "POINT_CLOUD_RECONSTRUCTION","thumbnail_s3_key": thumbnail_s3_key})
 
-            # Chiamata API a Gaussian Splatting
-            convert_request = {"input_dir": model_dir}
-            response = requests.post(GAUSSIAN_SPLATTING_API_URL + "/convert", json=convert_request)
-            if response.status_code != 200:
-                print(f"Errore durante la conversione: {response.text}")
-                return
+            self.build_point_cloud(model_dir)
 
             model_service.update_model_status(model_id, {"status": "MODEL_TRAINING"})
 
-            # Chiamata API per il training del modello
-            train_output_folder = os.path.join(model_dir, 'output')
-            train_request = {"input_dir": model_dir, "output_dir": train_output_folder}
-            response = requests.post(GAUSSIAN_SPLATTING_API_URL + "/train", json=train_request)
-            if response.status_code != 200:
-                print(f"Errore durante il training: {response.text}")
-                return
-
-            # Zippa la cartella output
-            zip_filename = os.path.join(model_dir, "3d_model.zip")
-            shutil.make_archive(zip_filename.replace('.zip', ''), 'zip', train_output_folder)
-
-            # Carica lo ZIP su S3
-            s3_key = f"models/{model_id}/3d_model.zip"
-            try:
-                repository_service.upload(zip_filename, s3_key)
-            except Exception as e:
-                print(f"Errore durante l'upload su S3: {e}")
-                # Se l'upload fallisce, metti lo stato in "FAILED" o altro per segnalarlo
-                model_service.update_model_status(model_id, {"status": "FAILED"})
-                return
-
-            if thumbnail_path and os.path.exists(thumbnail_path):
-                thumbnail_s3_key = f"models/{model_id}/thumbnail.jpg"
-    
-            thumbnail_s3_key = None
-
-            if thumbnail_path and os.path.exists(thumbnail_path):
-                try:
-                    thumbnail_s3_key = f"models/{model_id}/thumbnail.jpg"
-                    repository_service.upload(thumbnail_path, thumbnail_s3_key)
-                    print(f"✅ Thumbnail caricata su S3: {thumbnail_s3_key}")
-                except Exception as e:
-                    print(f"❌ Errore durante l'upload della thumbnail su S3: {e}")
-                    thumbnail_s3_key = None  # Se l'upload fallisce, non salviamo l'URL errato
-
+            output_s3_key = self.train_model(model_id,model_dir)
+            
             # 🔹 Sempre aggiorniamo lo stato con entrambi i percorsi
-            update_data = {
-                "status": "COMPLETED",
-                "output_s3_key": s3_key
-            }
-
-            if thumbnail_s3_key:
-                update_data["thumbnail_s3_key"] = thumbnail_s3_key
-
-            model_service.update_model_status(model_id, update_data)
+            model_service.update_model_status(model_id, {"status": "COMPLETED","output_s3_key": output_s3_key})
             
         finally:
                 # Questa parte verrà sempre eseguita, anche se c'è stata un'eccezione
                 ch.basic_ack(delivery_tag=method.delivery_tag)
                 print("Job completato o fallito, conferma del messaggio alla coda.")
+
+
+    def process_video(self,model_id,video_uri,model_dir):
+        video_file_path = os.path.join(model_dir, "video.mp4")
+        repository_service.download(video_uri, video_file_path)
+
+        # Estrarre fotogrammi dal video
+        frames_output_folder = os.path.join(model_dir, 'input')
+        thumbnail_path = self.extract_frames(video_file_path, frames_output_folder)
+
+        thumbnail_s3_key = None
+        if thumbnail_path and os.path.exists(thumbnail_path):
+            try:
+                thumbnail_s3_key = f"models/{model_id}/thumbnail.jpg"
+                repository_service.upload(thumbnail_path, thumbnail_s3_key)
+                print(f"✅ Thumbnail caricata su S3: {thumbnail_s3_key}")
+            except Exception as e:
+                print(f"❌ Errore durante l'upload della thumbnail su S3: {e}")
+                thumbnail_s3_key = None  # Se l'upload fallisce, non salviamo l'URL errato
+            return thumbnail_s3_key
+
+      
+    def build_point_cloud(self,model_dir):
+        # Chiamata API a Gaussian Splatting
+        convert_request = {"input_dir": model_dir}
+        response = requests.post(GAUSSIAN_SPLATTING_API_URL + "/convert", json=convert_request)
+        if response.status_code != 200:
+            print(f"Errore durante la conversione: {response.text}")
+            return
+        
+    def train_model(self,model_id,model_dir):
+        # Chiamata API per il training del modello
+        train_output_folder = os.path.join(model_dir, 'output')
+        train_request = {"input_dir": model_dir, "output_dir": train_output_folder}
+        response = requests.post(GAUSSIAN_SPLATTING_API_URL + "/train", json=train_request)
+        if response.status_code != 200:
+            print(f"Errore durante il training: {response.text}")
+            return
+
+        # Zippa la cartella output
+        zip_filename = os.path.join(model_dir, "3d_model.zip")
+        shutil.make_archive(zip_filename.replace('.zip', ''), 'zip', train_output_folder)
+
+        # Carica lo ZIP su S3
+        s3_key = f"models/{model_id}/3d_model.zip"
+        try:
+            repository_service.upload(zip_filename, s3_key)
+        except Exception as e:
+            print(f"Errore durante l'upload su S3: {e}")
+            # Se l'upload fallisce, metti lo stato in "FAILED" o altro per segnalarlo
+            model_service.update_model_status(model_id, {"status": "FAILED"})
+            return
+        return s3_key
+
 
     # Consuma i messaggi dalla coda
     def consume_jobs(self):
@@ -148,9 +153,9 @@ class QueueJobService:
 
         
     # Funzione per estrarre 120 immagini dal video
-    def extract_frames(self, video_path, output_folder, total_frames=120):
-        """Estrae fotogrammi da un video e salva il primo frame separatamente."""
-    
+    def extract_frames(self, video_path, output_folder,threshold=0.5):  # Aumenta total_frames
+        """Estrae fotogrammi ogni volta che c'è un cambiamento significativo nella scena"""
+
         os.makedirs(output_folder, exist_ok=True)
         cap = cv2.VideoCapture(video_path)
 
@@ -158,38 +163,46 @@ class QueueJobService:
             print(f"Errore: Impossibile aprire il video {video_path}")
             return None  # Se il video non si apre, ritorna None
 
-        total_video_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        frame_interval = max(1, total_video_frames // total_frames)  
-
-        frame_count = 0
+        previous_frame = None
         extracted_count = 0
-        first_frame_path = None  
+        first_frame_path = None
 
         while True:
             ret, frame = cap.read()
             if not ret:
-                break  
+                break
 
-            if frame_count % frame_interval == 0:
+            if previous_frame is None:
+                previous_frame = frame
+                continue
+
+            # Calcola la differenza tra il fotogramma corrente e quello precedente
+            diff = cv2.absdiff(previous_frame, frame)
+            gray_diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+            _, thresh = cv2.threshold(gray_diff, 30, 255, cv2.THRESH_BINARY)
+
+            # Calcola la percentuale di pixel che sono cambiati
+            change_percentage = np.sum(thresh) / (thresh.shape[0] * thresh.shape[1])
+
+            if change_percentage > threshold:
                 image_filename = os.path.join(output_folder, f"frame_{extracted_count + 1}.jpg")
-                cv2.imwrite(image_filename, frame)  
+                cv2.imwrite(image_filename, frame)
                 extracted_count += 1
 
                 if first_frame_path is None:
                     first_frame_path = image_filename  # Salva il primo frame estratto
-                
-            frame_count += 1
-            if extracted_count >= total_frames:
-                break  
+
+            previous_frame = frame  # Imposta il fotogramma corrente come fotogramma precedente
 
         cap.release()
-        
+
         if first_frame_path:
             print(f"✅ Primo frame salvato: {first_frame_path}")
         else:
             print("❌ Nessun frame salvato.")
 
-        return first_frame_path  # ✅ Restituisce il primo frame
+        return first_frame_path  # Restituisce il primo frame estratto
+
 
 
     def handle_exit(self, signum, frame):
